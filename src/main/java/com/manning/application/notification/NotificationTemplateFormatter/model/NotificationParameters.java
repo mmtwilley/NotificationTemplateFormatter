0a9054beb84de8695eae4749778f149f3f86d900
Marvin Twilley <marvinmtwilley@gmail.com>
2023-10-24 23:05:47 -0500

    Added to config, model, and applications

---
+++ src/main/java/com/manning/application/notification/NotificationTemplateFormatter/model/NotificationParameters.java
@@ -0,0 +1,21 @@
+package com.manning.application.notification.NotificationTemplateFormatter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class NotificationParameters {
+
+    private String NotificationParameterName;
+    private String NotificationParameterValue;
+
+    @Override
+    public String toString() {
+        return "NotificationParameters{" +
+                "NotificationParameterName:'" + NotificationParameterName + '\'' +
+                ", NotificationParameterValue:'" + NotificationParameterValue + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/manning/application/notification/NotificationTemplateFormatter/model/NotificationTemplateRequest.java
@@ -0,0 +1,16 @@
+package com.manning.application.notification.NotificationTemplateFormatter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class NotificationTemplateRequest {
+    private List<NotificationParameters> notificationParameters;
+    private String notificationTemplateName;
+    private String notificationMode;
+}
